1b091427ffa2081e451999839fcccf2d5496d5f6
Aneja <91323255+AnejaPirnat@users.noreply.github.com>
2023-04-03 14:08:49 +0200

    users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "services#index"
+  get '/register', to: 'users#new', as: 'new_user_registration'
+  post '/register', to: 'users#create', as: 'user_registration'
+
 end
